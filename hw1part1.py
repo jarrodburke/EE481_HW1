c6c0e35f438cd622bdf46f184bc05d77373c7d29
Connor Jordan <connor.jordan4040@gmail.com>
2023-09-10 16:25:19 -0700

    no desert >:(

--- hw1part1.py
+++ hw1part1.py
@@ -5,6 +5,8 @@ import numpy as np
 image_path = "Winter.png"
 image = cv2.imread(image_path)
 
+
+
 norm_image = cv2.normalize(image,None, 0, 255, cv2.NORM_MINMAX)
 
 norm_image_bw = cv2.cvtColor(norm_image,cv2.COLOR_BGR2GRAY)
@@ -33,5 +35,31 @@ plt.xlim(0, 256)  # Set the x-axis limit to the range of pixel values (0 to 256)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
 
+crop_img = norm_image_bw[400:500, 400:500]
+cv2.imshow("crop", crop_img)
+cv2.waitKey(0)
 
-cv2.imshow(norm_image)
+hist = cv2.calcHist([crop_img], [0], None, [256], [0, 256])
+
+# Normalize the histogram
+
+# Plot the histogram in black and white
+plt.figure(figsize=(8, 6))
+plt.hist(crop_img.ravel(), bins=256, range=(0, 256), color='black', alpha=0.7)
+plt.title('Histogram of Normalized Image (Black and White)')
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+plt.xlim(0, 256)  # Set the x-axis limit to the range of pixel values (0 to 256)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plt.hist(norm_image_bw.ravel(), bins=256, range=(0, 256), color='black', alpha=0.5, label='Grayscale Image')
+plt.hist(crop_img.ravel(), bins=256, range=(0, 256), color='red', alpha=0.5, label='Cropped Region')
+plt.title('Histograms of Grayscale Image and Cropped Region')
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+plt.xlim(0, 256)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.legend()
+plt.show()
