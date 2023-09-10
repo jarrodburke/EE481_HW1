c2237aa9773cea3a9be0831b3c342568368e54fe
Connor Jordan <connor.jordan4040@gmail.com>
2023-09-10 16:38:14 -0700

    no desert >:(

--- hw1part1.py
+++ hw1part1.py
@@ -23,15 +23,14 @@ cv2.destroyAllWindows()
 
 hist = cv2.calcHist([norm_image_bw], [0], None, [256], [0, 256])
 
-# Normalize the histogram
 
-# Plot the histogram in black and white
+
 plt.figure(figsize=(8, 6))
 plt.hist(norm_image_bw.ravel(), bins=256, range=(0, 256), color='black', alpha=0.7)
 plt.title('Histogram of Normalized Image (Black and White)')
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
-plt.xlim(0, 256)  # Set the x-axis limit to the range of pixel values (0 to 256)
+plt.xlim(0, 256)  
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
 
@@ -41,15 +40,13 @@ cv2.waitKey(0)
 
 hist = cv2.calcHist([crop_img], [0], None, [256], [0, 256])
 
-# Normalize the histogram
 
-# Plot the histogram in black and white
 plt.figure(figsize=(8, 6))
 plt.hist(crop_img.ravel(), bins=256, range=(0, 256), color='black', alpha=0.7)
 plt.title('Histogram of Normalized Image (Black and White)')
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
-plt.xlim(0, 256)  # Set the x-axis limit to the range of pixel values (0 to 256)
+plt.xlim(0, 256) 
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
 
